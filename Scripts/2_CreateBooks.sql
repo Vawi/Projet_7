f209cb1d8f9d6c35737840675aec5bf6e0af5cc4
Vawi <val.winnen@gmail.com>
2018-08-06 16:17:56 +0200

    correction
    petites corrections

--- Scripts/2_CreateBooks.sql
+++ Scripts/2_CreateBooks.sql
@@ -4,7 +4,7 @@ CREATE TABLE public.ouvrage (
   id_ouvrage INTEGER NOT NULL DEFAULT nextval('public.ouvrage_id_ouvrage_seq'),
   titre VARCHAR(100) NOT NULL,
   auteur VARCHAR(100) NOT NULL,
-  nombre_disponible INTEGER NOT NULL DEFAULT,
+  nombre_disponible INTEGER NOT NULL,
   CONSTRAINT ouvrage_pk PRIMARY KEY (id_ouvrage)
 );
 
