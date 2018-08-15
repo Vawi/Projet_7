6b7bec58afc626b484e3d9c3162318610d38d4fe
Vawi <val.winnen@gmail.com>
2018-08-15 19:54:47 +0200

    Test + WSDL
    Tentative test et generation WSDL

--- Projet_7_consumer/src/test/java/org/val/win/consumer/impl/dao/OuvrageDaoImplTest.java
+++ Projet_7_consumer/src/test/java/org/val/win/consumer/impl/dao/OuvrageDaoImplTest.java
@@ -3,12 +3,23 @@ package org.val.win.consumer.impl.dao;
 import org.junit.jupiter.api.Test;
 import org.val.win.consumer.contract.dao.OuvrageDao;
 import org.val.win.model.bean.Ouvrage;
+import org.junit.runner.RunWith;
+import org.springframework.util.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import org.val.win.model.exception.NotFoundException;
 
 import javax.inject.Inject;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:/consumerContext.xml"})
+@Rollback(true)
 class OuvrageDaoImplTest {
 
     @Inject
@@ -16,6 +27,7 @@ class OuvrageDaoImplTest {
 
     @Test
     void getListOuvrage() {
-
+        List<Ouvrage> listOuvrage = ouvrageDao.getListOuvrage();
+        assertEquals(listOuvrage.size(), 29);
     }
 }
